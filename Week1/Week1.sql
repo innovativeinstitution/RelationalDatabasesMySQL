216b81adae89a3016a9f5b9348564470b9b9752e
Manish Narayan <manishnarayan2380@gmail.com>
2021-04-14 17:40:12 -0700

    Week1 Notes

---
+++ Week1/Week1.sql
@@ -0,0 +1,25 @@
+use university;
+
+insert into student (firstname, lastname, dob)
+values ('Manish', ' Narayan', '1980-02-03');
+insert into student (firstname, lastname, dob)
+values ('David', ' Rossi', '1995-01-10');
+insert into student (firstname, lastname, dob)
+values ('John', 'Doe', '1965-07-21');
+
+
+select * from student;
+select firstname, lastname from student;
+
+select * from student order by dob;
+
+select * from student where lastname like '%e%';
+
+select * from student where (studentid = 2 or studentid = 3) order by lastname desc;
+
+delete from student where studentid = 2;
+
+update student set lastname = 'Narayana' where studentid = 1
+
+select * from student;
+
